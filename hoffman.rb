58d4dc45815507deb64a36c161a5a0fae97c9ec6
Erik Landmark <erik.landmark@itggot.se>
2019-01-18 00:56:11 +0100

    update

--- hoffman.rb
+++ hoffman.rb
@@ -1,17 +1,14 @@
 #require pp'
 require 'stackprof'
+require "benchmark"
 
-time = Time.now
+#time = Time.now
 
-#text = File.read("long_text.txt")
-text = "this is an example of a huffman tree"
+text = File.read("long_text.txt")
+#text = "this is an example of a huffman tree"
 
 class Node
-  attr_accessor :left, :right, :symbol, :freq
-  @left = nil
-  @right = nil
-  @symbol = ""
-  @frequency = 0
+  attr_accessor :left, :right, :symbol, :frequency
 
   def initialize(left, right, symbol, freq)
     @left = left
@@ -31,28 +28,30 @@ end
 
 def get_table(text)
   chars_map = text.chars.reduce({}) {|acc, char| acc[char] = acc[char].nil? ? 1 : acc[char] + 1; acc}
-  @nodes = chars_map.sort_by{|_,v|v}
+  @nodes = chars_map.sort_by {|_, v| v}
   @tree = Node.new(nil, nil, "", text.length)
   create_tree(@tree, 1, Math.log2(@nodes.length).ceil)
-  chars_map.keys.reduce({}){|acc, sym| get_path("0", @tree.left, sym); get_path("1", @tree.right, sym); acc[sym] = @path; @path = ""; acc}
+  chars_map.keys.reduce({}) {|acc, sym| get_path("0", @tree.left, sym); get_path("1", @tree.right, sym); acc[sym] = @path; @path = ""; acc}
 end
 
 def create_tree(node, layer, max_layer)
   if layer >= max_layer
     n = @nodes.delete_at(0)
     unless n.nil?
-      node.left = Node.new(nil, nil , n[0], n[1])
+      node.left = Node.new(nil, nil, n[0], n[1])
     end
     n = @nodes.delete_at(0)
     unless n.nil?
-      node.right = Node.new(nil, nil , n[0], n[1])
+      node.right = Node.new(nil, nil, n[0], n[1])
     end
   else
-    if @nodes.length >= 2
+    if @nodes.length == 1
+      n = @nodes.delete_at(0)
+      node.symbol = n[0]
+      node.frequency = n[1]
+    elsif @nodes.length >= 2
       node.left = Node.new(nil, nil, "", 0)
       create_tree(node.left, layer + 1, max_layer)
-    end
-    if @nodes.length >= 1
       node.right = Node.new(nil, nil, "", 0)
       create_tree(node.right, layer + 1, max_layer)
     end
@@ -60,7 +59,9 @@ def create_tree(node, layer, max_layer)
 end
 
 def encode(path, text)
-  text.chars.reduce([]){|acc, char| acc << path[char]}.join("")
+  File.open("encoded", "wb") do |file|
+    file.write(text.chars.reduce([]) {|acc, char| acc << path[char]}.join("").scan(/.{8}/).reduce([]) { |acc, byte| acc << byte.to_i(2); acc}.pack("C*"))
+  end
 end
 
 def get_path(path, node, target)
@@ -76,66 +77,46 @@ def get_path(path, node, target)
   end
 end
 
-def decode(path, stream)
-  decoded_array = []
-  StackProf.run(mode: :object, out: 'stackprof.dump') do
-
+def decode(path)
+  byte = ""
   path = path.invert
-  stream.chars.reduce("") do |acc, char|
-    acc += char
-    unless path[acc].nil?
-      decoded_array << path[acc]
-      acc = ""
+  decoded_array = IO.binread("encoded").unpack("B*").join("").chars.reduce([]) do |acc, char|
+    byte += char
+    unless path[byte].nil?
+      acc << path[byte]
+      byte = ""
     end
     acc
-  end
   end
 
   decoded_array.join("")
 end
 
-def write_to_file(stream)
-  file_buff = []
-  (stream.length / 8.0).ceil.times do |i|
-    byte = (stream[i...i + (8 * i > stream.length ? 8 * i - stream.length : 8)].to_i(2))
-    file_buff << byte
-  end
+encoded = ""
 
-  File.binwrite("encoded", file_buff.pack("C*"))
-end
+measure = Benchmark.measure {
+  print "Original length (bytes): "
+  original_length = text.length
+  puts original_length
 
-print "Original length (bytes): "
-original_length = text.length
-puts original_length
+  print "Creating node tree... "
+  path = get_table(text)
+  puts "Done!"
+  print "Starting encoding... "
+  encoded = encode(path, text)
+  puts "Done!"
+  print "Encoded length (bytes): "
+  puts encoded
 
-
-print "Creating node tree... "
-path = get_table(text)
-puts "Done!"
-
-print "Starting encoding... "
-encoded = encode(path, text)
-puts "Done!"
-print "Encoded length (bytes): "
-encoded_length = (encoded.length / 8.0).ceil
-puts encoded_length
-
-write_to_file(encoded)
-
-print "Starting decoding... "
-  decoded = decode(path, encoded)
+  print "Starting decoding... "
+  decoded = decode(path)
   puts "Done!"
   print "Decoded length (bytes): "
   puts decoded.length
   IO.write("decoded", decoded)
-puts decoded
-
-
-
+}
 
 puts "============"
-puts "Total time: " + time_diff_milli(time, Time.now).to_s + " seconds"
-puts "Compression ratio: " + ((1 -(encoded_length.to_f / original_length)) * 100).round(2).to_s + "%"
-
-
-
+#puts "Total time: " + time_diff_milli(time, Time.now).to_s + " seconds"
+puts "Compression ratio: " + ((1 - (encoded.to_f / text.length.to_f)) * 100).round(2).to_s + "%"
+puts measure
